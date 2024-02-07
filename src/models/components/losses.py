b3a2a1a35329767a229608eb086ff5e991780f01
Igor Krashenyi <reachingxforward@gmail.com>
2024-02-07 12:00:18 +0200

    init

--- src/models/components/losses.py
+++ src/models/components/losses.py
@@ -246,69 +246,6 @@ def get_map(mask):
     return mask
 
 
-# def test_single_volume(
-#     image,
-#     label,
-#     net,
-#     classes,
-#     patch_size=[256, 256],
-#     test_save_path=None,
-#     case=None,
-#     z_spacing=1,
-# ):
-#     image, label = (
-#         image.squeeze(0).cpu().detach().numpy(),
-#         label.squeeze(0).cpu().detach().numpy(),
-#     )
-#     if len(image.shape) == 3:
-#         prediction = np.zeros_like(label)
-#         for ind in range(image.shape[0]):
-#             slice = image[ind, :, :]
-#             x, y = slice.shape[0], slice.shape[1]
-#             if x != patch_size[0] or y != patch_size[1]:
-#                 slice = zoom(
-#                     slice, (patch_size[0] / x, patch_size[1] / y), order=3
-#                 )  # previous using 0
-#             input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
-#             net.eval()
-#             with torch.no_grad():
-#                 outputs = net(input)
-#                 out = torch.argmax(torch.softmax(outputs, dim=1), dim=1).squeeze(0)
-#                 out = out.cpu().detach().numpy()
-#                 if x != patch_size[0] or y != patch_size[1]:
-#                     pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
-#                 else:
-#                     pred = out
-#                 prediction[ind] = pred
-#     else:
-#         input = torch.from_numpy(image).unsqueeze(0).unsqueeze(0).float().cuda()
-#         net.eval()
-#         with torch.no_grad():
-#             out = torch.argmax(torch.softmax(net(input), dim=1), dim=1).squeeze(0)
-#             prediction = out.cpu().detach().numpy()
-#     metric_list = []
-#     big_list = []
-#     small_list = []
-#     for i in range(1, classes):
-#         metric_list.append(calculate_metric_percase(prediction == i, label == i))
-#         if _adaptive_size(label == i) >= 0.2:
-#             small_list.append(metric_list[-1][0])
-#         else:
-#             big_list.append(metric_list[-1][0])
-#
-#     if test_save_path is not None:
-#         img_itk = sitk.GetImageFromArray(image.astype(np.float32))
-#         prd_itk = sitk.GetImageFromArray(prediction.astype(np.float32))
-#         lab_itk = sitk.GetImageFromArray(label.astype(np.float32))
-#         img_itk.SetSpacing((1, 1, z_spacing))
-#         prd_itk.SetSpacing((1, 1, z_spacing))
-#         lab_itk.SetSpacing((1, 1, z_spacing))
-#         sitk.WriteImage(prd_itk, test_save_path + "/" + case + "_pred.nii.gz")
-#         sitk.WriteImage(img_itk, test_save_path + "/" + case + "_img.nii.gz")
-#         sitk.WriteImage(lab_itk, test_save_path + "/" + case + "_gt.nii.gz")
-#     return metric_list, big_list, small_list
-
-
 class BoundaryDoULoss(nn.Module):
     def __init__(self, n_classes=1):
         super(BoundaryDoULoss, self).__init__()
