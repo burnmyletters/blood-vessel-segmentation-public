48cd38cac82c98bb91f2fe3bdc5fe86acce3a878
Igor Krashenyi <reachingxforward@gmail.com>
2024-02-11 17:29:47 +0200

    Update generate_mutliview_pseudo.py

--- scripts/generate_mutliview_pseudo.py
+++ scripts/generate_mutliview_pseudo.py
@@ -3,10 +3,9 @@ import glob
 import numpy as np
 import cv2
 import tqdm
+import argparse 
 
 TESTING = False
-
-dataset_path = "mnt/working/blood-vessel-segmentation/"
 
 
 def load_volume(dataset):
@@ -41,26 +40,36 @@ def save_volume(volume, target, dataset):
         cv2.imwrite(os.path.join(dataset, "images", f"{z:04d}.jp2"), volume[z])
         cv2.imwrite(os.path.join(dataset, "labels", f"{z:04d}.png"), target[z])
 
+def main():
+    for dataset in glob.glob(os.path.join(dataset_path, "train_ext", "*")):
+        dataset_xz = dataset + "_xz"
+        dataset_zy = dataset + "_zy"
+        skip_xz = os.path.exists(dataset_xz)
+        skip_zy = os.path.exists(dataset_zy)
+        if not skip_xz or not skip_zy:
+            print("load", dataset)
+            volume, target = load_volume(dataset)
+        if not skip_xz:
+            print("save", dataset_xz)
+            volume_xz, target_xz = volume.transpose((1, 2, 0)), target.transpose((1, 2, 0))
+            if not TESTING:
+                save_volume(volume_xz, target_xz, dataset_xz)
+        else:
+            print("skipping", dataset_xz)
+        if not skip_zy:
+            print("save", dataset_zy)
+            volume_zy, target_zy = volume.transpose(2, 0, 1), target.transpose(2, 0, 1)
+            if not TESTING:
+                save_volume(volume_zy, target_zy, dataset_zy)
+        else:
+            print("skipping", dataset_zy)
 
-for dataset in glob.glob(os.path.join(dataset_path, "train_ext", "*")):
-    dataset_xz = dataset + "_xz"
-    dataset_zy = dataset + "_zy"
-    skip_xz = os.path.exists(dataset_xz)
-    skip_zy = os.path.exists(dataset_zy)
-    if not skip_xz or not skip_zy:
-        print("load", dataset)
-        volume, target = load_volume(dataset)
-    if not skip_xz:
-        print("save", dataset_xz)
-        volume_xz, target_xz = volume.transpose((1, 2, 0)), target.transpose((1, 2, 0))
-        if not TESTING:
-            save_volume(volume_xz, target_xz, dataset_xz)
-    else:
-        print("skipping", dataset_xz)
-    if not skip_zy:
-        print("save", dataset_zy)
-        volume_zy, target_zy = volume.transpose(2, 0, 1), target.transpose(2, 0, 1)
-        if not TESTING:
-            save_volume(volume_zy, target_zy, dataset_zy)
-    else:
-        print("skipping", dataset_zy)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process blood vessel segmentation data.')
+    parser.add_argument('dataset_path', type=str, help='Path to the dataset directory')
+    args = parser.parse_args()
+    
+    dataset_path = args.dataset_path
+    main()
